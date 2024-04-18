5141a4483292ade5837fe05173ed9ce0e1be2fdf
Ramiro <ramiro174@gmail.com>
2024-04-17 20:44:33 -0700

    finish

--- app/src/main/java/com/example/practicaiot/view/MainActivity.java
+++ app/src/main/java/com/example/practicaiot/view/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.practicaiot.view;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,52 +24,83 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
-    Retrofit  retrofit;
+    Retrofit retrofit;
     ApiAdafruit apiAdafruit;
 
     int Encendido;
+    Button b, btnultrasonico;
+
+    TextView txtultrasonico;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        retrofit= RetrofitBuilder.getRetrofitInstance();
-        ApiAdafruit apiAdafruit= retrofit.create(ApiAdafruit.class);
-         findViewById(R.id.btnLed).setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Feed  f=new Feed();
-                 if(Encendido==1) {
-                     f.setValue("1");
-                     Encendido=0;
-                 }else{
-                     f.setValue("0");
-                     Encendido=1;
-                 }
+        retrofit = RetrofitBuilder.getRetrofitInstance();
+        ApiAdafruit apiAdafruit = retrofit.create(ApiAdafruit.class);
+        b=findViewById(R.id.btnLed);
+        btnultrasonico=findViewById(R.id.btnultrasonico);
+        txtultrasonico=findViewById(R.id.txtultrasonico);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Feed f = new Feed();
+                if (Encendido == 1) {
+                    f.setValue("1");
+                    Encendido = 0;
+                } else {
+                    f.setValue("0");
+                    Encendido = 1;
+                }
+                Call<Feed> request = apiAdafruit.setDataFeed("led", f);
+                request.enqueue(new Callback<Feed>() {
+                    @Override
+                    public void onResponse(Call<Feed> call, Response<Feed> response) {
+                        if (response.isSuccessful()) {
+                            Feed feed = response.body();
+                            if(feed.getValue().equals("0")){
+                                b.setText("Encender");
+                            }else{
+                                b.setText("Apagar");
+                            }
+                            Toast.makeText(MainActivity.this, feed.getValue(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<Feed> call, Throwable t) {
+                        Log.i("onFailure: ", t.getMessage());
+
+                    }
+                });
 
 
-                 Call<Feed>  request= apiAdafruit.setDataFeed("led",f);
+            }
+        });
 
-                 request.enqueue(new Callback<Feed>() {
-                     @Override
-                     public void onResponse(Call<Feed> call, Response<Feed> response) {
+        btnultrasonico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<Feed> request = apiAdafruit.getDataFeedLast("ultrasonico");
+                request.enqueue(new Callback<Feed>() {
+                    @Override
+                    public void onResponse(Call<Feed> call, Response<Feed> response) {
+                        if(response.isSuccessful()){
 
-                         if(response.isSuccessful()){
-                             Feed  Values= response.body();
-                             Log.i("datos",Values.getValue()+"");
+                            Feed feed=response.body();
+                            txtultrasonico.setText(feed.getValue());
 
-                         }
+                        }
 
-                     }
+                    }
+                    @Override
+                    public void onFailure(Call<Feed> call, Throwable t) {
 
-                     @Override
-                     public void onFailure(Call<Feed> call, Throwable t) {
-                         Log.i("onFailure: ",t.getMessage());
+                    }
+                });
 
-                     }
-                 });
-
-
-             }
-         });
+            }
+        });
     }
 }
